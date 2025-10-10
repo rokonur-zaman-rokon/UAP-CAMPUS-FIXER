dc122b5b96d117c22740052936642e7fdb2ec44c
MD Shajalal <mdshajalallikhon@gmail.com>
2025-10-11 03:11:07 +0600

    some changes

--- campus_fixer/asgi.py
+++ campus_fixer/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uapcampusfixer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campus_fixer.settings')
 
 application = get_asgi_application()
